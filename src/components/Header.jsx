20c3ab49b6290a29d3ae95948eeb7cbf92e34ffe
ali tarichi <tarichiali@gmail.com>
2024-03-04 19:39:57 +0300

    navbar initated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import { brainwave } from "../assets";
+import { navigation } from "../constants";
 
 const Headrer = () => {
   return (
@@ -10,20 +11,24 @@ const Headrer = () => {
       <div
         className="flex item-center px-5 lg:px-7.5
       xl:px-10 max-lg:py-4"
-      ></div>
-      <a className="block w-[12rem] xl:mr-8" href="#hero">
-        <img src={brainwave} width={190} height={60} alt="Brainwave" />
-      </a>
-
-      <nav
-        className="hidden fixed top-[5rem] left-0
-      right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent"
       >
-        <div
-          className="relative z-2 flex flex-col
-        items-center justify-center m-auto lg:flex-row"
-        ></div>
-      </nav>
+        <a className="block w-[12rem] xl:mr-8" href="#hero">
+          <img src={brainwave} width={190} height={60} alt="Brainwave" />
+        </a>
+
+        <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+          <div
+            className="relative z-2 flex flex-col
+          items-center justify-center m-auto lg:flex-row"
+          >
+            {navigation.map((items) => (
+              <a key={items.id} href={items.url}>
+                {items.title}
+              </a>
+            ))}
+          </div>
+        </nav>
+      </div>
     </div>
   );
 };
