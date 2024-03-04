ffd59c209df265322952d9d2cd57820694add49c
ali tarichi <tarichiali@gmail.com>
2024-03-04 17:40:16 +0300

    constants created and added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,31 @@
+import { brainwave } from "../assets";
+
 const Headrer = () => {
-  return <div>Headrer</div>;
+  return (
+    <div
+      className="fixed top-0 z-50 bg-n-8/90
+    backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90
+    lg:backdrop-blur-sm"
+    >
+      <div
+        className="flex item-center px-5 lg:px-7.5
+      xl:px-10 max-lg:py-4"
+      ></div>
+      <a className="block w-[12rem] xl:mr-8" href="#hero">
+        <img src={brainwave} width={190} height={60} alt="Brainwave" />
+      </a>
+
+      <nav
+        className="hidden fixed top-[5rem] left-0
+      right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent"
+      >
+        <div
+          className="relative z-2 flex flex-col
+        items-center justify-center m-auto lg:flex-row"
+        ></div>
+      </nav>
+    </div>
+  );
 };
 
 export default Headrer;
