13601be68edfd23289a27acfdaf39f396d5e0bba
ali tarichi <tarichiali@gmail.com>
2024-03-06 14:05:35 +0300

    header alignment fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,11 @@ const Headrer = () => {
 
   return (
     <div
-      className="fixed top-0 left-0 w-full z-50 bg-n-8/90
-      backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90
-      lg:backdrop-blur-sm"
+      className="fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90
+      lg:backdrop-blur-sm bg-n-8/90 backdrop-blur-sm"
     >
       <div
-        className="flex item-center px-5 lg:px-7.5
+        className="flex items-center px-5 lg:px-7.5
       xl:px-10 max-lg:py-4"
       >
         <a className="block w-[12rem] xl:mr-8" href="#hero">
@@ -28,21 +27,19 @@ const Headrer = () => {
             className="relative z-2 flex flex-col
           items-center justify-center m-auto lg:flex-row"
           >
-            {navigation.map((items) => (
+            {navigation.map((item) => (
               <a
-                key={items.id}
-                href={items.url}
-                className={`block relative font-code text-2xl 
-                uppercase text-n-1 transition-colors hover:text-color-1
-                ${items.onlyMobile ? "lg:hidden" : ""}
-                px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold 
-                ${
-                  items.url === pathname.hash
+                key={item.id}
+                href={item.url}
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+                  item.onlyMobile ? "lg:hidden" : ""
+                } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                  item.url === pathname.hash
                     ? "z-2 lg:text-n-1"
                     : "lg:text-n-1/50"
-                } lg:leading-5 lg:hover:text-n-1 xl:px-12 `}
+                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
               >
-                {items.title}
+                {item.title}
               </a>
             ))}
           </div>
@@ -53,10 +50,10 @@ const Headrer = () => {
           className="button hidden mr-8 text-n-1/50 
         transition-colors hover:text-n-1 lg:block"
         >
-          New Account
+          New account
         </a>
         <Button className="hidden lg:flex" href="#login">
-          Sign In
+          Sign in
         </Button>
       </div>
     </div>
