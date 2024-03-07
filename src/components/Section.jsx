a392a40d8ab5b3c1c2561402d7584f5c92acdc03
ali tarichi <tarichiali@gmail.com>
2024-03-07 13:09:28 +0300

    section lines

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -18,6 +18,17 @@ const Section = ({
       }`}
     >
       {children}
+
+      <div
+        className="hidden absolute top-0 left-5 w-0.25
+      h-full bg-stroke-1 pointer-events-none md:block
+      lg:left-7.5 xl:left-10 "
+      ></div>
+      <div
+        className="hidden absolute top-0 left-5 w-0.25
+      h-full bg-stroke-1 pointer-events-none md:block
+      lg:right-7.5 xl:right-10 "
+      ></div>
     </div>
   );
 };
