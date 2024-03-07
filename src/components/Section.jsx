4dc534d15814109d3c3ac8676f52dab123dd5b9e
ali tarichi <tarichiali@gmail.com>
2024-03-07 13:22:31 +0300

    hero initiated

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -10,11 +10,12 @@ const Section = ({
 }) => {
   return (
     <div
+      id={id}
       className={`relative ${
         customPaddings ||
         `py-10
-  lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}
-   ${className || ""}`
+        lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}
+        ${className || ""}`
       }`}
     >
       {children}
