3e5dfe5acb80672c38bc725e10fca49d9e6744a8
ali tarichi <tarichiali@gmail.com>
2024-03-07 13:05:39 +0300

    initalchildren in section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -8,8 +8,18 @@ const Section = ({
   customPaddings,
   children,
 }) => {
-  return <div className={`relative ${customPaddings || `py-10
-  lg:py-16 xl:py-20 ${crosses ? 'lg:py-32 xl:py-40 : ''}`}`} ></div>;
+  return (
+    <div
+      className={`relative ${
+        customPaddings ||
+        `py-10
+  lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}
+   ${className || ""}`
+      }`}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Section;
