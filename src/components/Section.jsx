02e5f3f57e55101ec0f451a6413483b69b7a7c2d
ali tarichi <tarichiali@gmail.com>
2024-03-07 13:01:09 +0300

    intiated section

---
+++ src/components/Section.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Section = ({
+  className,
+  id,
+  crosses,
+  crossOffset,
+  customPaddings,
+  children,
+}) => {
+  return <div className={`relative ${customPaddings || `py-10
+  lg:py-16 xl:py-20 ${crosses ? 'lg:py-32 xl:py-40 : ''}`}`} ></div>;
+};
+
+export default Section;
