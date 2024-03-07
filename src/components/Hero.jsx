ed8ec607bc1dfde9fb5c407e74b572bba67a29f8
ali tarichi <tarichiali@gmail.com>
2024-03-07 14:11:36 +0300

    hero started

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import Section from "./Section";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <Section
+      className="pt-[12rem] -mt-[5.25]"
+      crosses
+      crossOffset="lg:translate-y-[5.25rem]"
+      customPaddings
+      id="hero"
+    >
+      <div></div>
+    </Section>
+  );
 };
 
 export default Hero;
