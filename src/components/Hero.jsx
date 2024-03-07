a2c8591098388572db666092ed01e32076f17a18
ali tarichi <tarichiali@gmail.com>
2024-03-07 14:26:28 +0300

    button added to hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { curve } from "../assets";
+import Button from "./Button";
 import Section from "./Section";
 
 const Hero = () => {
@@ -13,7 +14,7 @@ const Hero = () => {
       <div className="container relative">
         <div
           className="relative z-1 max-w-[62rem] 
-        mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]"
+        mx-auto text-center mb-[4rem] md:mb-20 lg:mb:[6rem]"
         >
           <h1 className="h1 mb-6">
             Explore the Possibilities of AI chatting with BrainwaveCurve
@@ -29,6 +30,16 @@ const Hero = () => {
               />
             </span>
           </h1>
+          <p
+            className="body-1 max-w-3xl mx-auto mb-6
+          text-n-2 lg:mb-8 "
+          >
+            Unleash the power of AI within Brainwave. Upgrade your productivity
+            with Brainwave, the open AI chat app.
+          </p>
+          <Button href="/pricing" white>
+            Get started
+          </Button>
         </div>
       </div>
     </Section>
