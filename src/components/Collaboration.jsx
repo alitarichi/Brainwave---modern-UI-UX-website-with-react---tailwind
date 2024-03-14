4c6977d24b2a5f05b99a73ab4f0828a063b9c9b6
ali tarichi <tarichiali@gmail.com>
2024-03-14 16:31:27 +0300

    list begin in collab

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -13,7 +13,8 @@ const Collaboration = () => {
             {collabContent.map((item) => (
               <li className="mb-3 py-3" key={item.id}>
                 <div className="flex items-center">
-                  <img src={check} />
+                  <img src={check} width={24} height={24} alt="check" />
+                  <h6 className="body-2 ml-5">{item.title}</h6>
                 </div>
               </li>
             ))}
