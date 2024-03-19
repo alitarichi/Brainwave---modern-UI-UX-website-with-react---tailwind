f38de76b30cf8cc535168799d83851822c35dad5
ali tarichi <tarichiali@gmail.com>
2024-03-19 12:45:22 +0300

    tagline component initated

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { smallSphere, stars } from "../assets";
+import Heading from "./Heading";
+import Section from "./Section";
 
 const Pricing = () => {
-  return <div>Pricing</div>;
+  return (
+    <Section className="overflow-hidden" id="pricing">
+      <div className="container relative z-2">
+        <div className="hidden relative justify-center mb-[6.5rem] lg:flex">
+          <img
+            src={smallSphere}
+            className="relative z-1"
+            width={255}
+            height={255}
+            alt="sphere"
+          />
+          <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
+            <img
+              src={stars}
+              className="w-full"
+              width={950}
+              height={400}
+              alt="stars"
+            />
+          </div>
+        </div>
+        <Heading
+          className=""
+          tag="Get started with brainwave"
+          title="Pay Once, Use Forever"
+        />
+      </div>
+    </Section>
+  );
 };
 
 export default Pricing;
---
+++ src/components/Tagline.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tagline = () => {
+  return <div>Tagline</div>;
+};
+
+export default Tagline;
