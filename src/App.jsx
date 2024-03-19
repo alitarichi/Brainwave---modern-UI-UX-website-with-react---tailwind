603310e603d2412f52d854303db744798783c78b
ali tarichi <tarichiali@gmail.com>
2024-03-19 12:32:19 +0300

    pricing section inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Button from "./components/Button";
 import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
 import Services from "./components/Services";
 
 const App = () => {
@@ -17,6 +18,7 @@ const App = () => {
       </div>
       <ButtonGradient />
       <Services />
+      <Pricing />
     </>
   );
 };
