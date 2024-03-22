9b5f6e0ebcbe7e459c34797c8c4e00e4268817bc
ali tarichi <tarichiali@gmail.com>
2024-03-22 12:33:32 +0300

    pricing section completed

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -4,7 +4,7 @@ const Heading = ({ className, title, text, tag }) => {
   return (
     <div
       className={`${className} max-w-[50rem] mx-auto
-    mb-12 lg:mb-20`}
+    mb-12 lg:mb-20 md:text-center`}
     >
       {tag && <Tagline className="mb-4 md:justify-center">{tag}</Tagline>}
       {title && <h2 className="h2">{title}</h2>}
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -37,6 +37,15 @@ const Pricing = () => {
           <LeftLine />
           <RightLine />
         </div>
+
+        <div className="flex justify-center mt-10">
+          <a
+            className="text-xs font-code font-bold tracking-wider uppercase border-b"
+            href="/pricing"
+          >
+            See full details
+          </a>
+        </div>
       </div>
     </Section>
   );
