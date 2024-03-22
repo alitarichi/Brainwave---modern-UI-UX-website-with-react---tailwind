96dbf670f7304df480031fee68594730ea420595
ali tarichi <tarichiali@gmail.com>
2024-03-22 14:20:02 +0300

    footer fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,34 @@
+import { socials } from "../constants";
+import Section from "./Section";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Section crosses className="!px-0 !py-10">
+      <div
+        className="container flex sm:justify-between
+      justify-center items-center gap-10"
+      >
+        <p className="caption text-n-4 lg:block">
+          ©{new Date().getFullYear()}. All Rights Reserved.
+        </p>
+
+        <ul className="flex gap-5 flex-wrap">
+          {socials.map((item) => (
+            <a
+              key={item.id}
+              href={item.url}
+              target="_blank"
+              className="flex 
+            items-center justify-center w-10 h-10 bg-n-7 rounded-full *:transition-colors
+            hover:bg-n-6"
+            >
+              <img src={item.iconUrl} width={16} height={16} alt={item.title} />
+            </a>
+          ))}
+        </ul>
+      </div>
+    </Section>
+  );
 };
 
 export default Footer;
