85a3a457c1de761c5320cd823444ea08dd4c08c3
ali tarichi <tarichiali@gmail.com>
2024-03-22 13:08:19 +0300

    large devices fixed on roadmap

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -15,9 +15,18 @@ const Roadmap = () => {
             const status = item.status === "done" ? "Done" : "in progress";
 
             return (
-              <div key={item.id}>
-                <div>
-                  <div>
+              <div
+                key={item.id}
+                className={`md:flex even:translate-y-[7rem] p-0.25
+              rounded-[2.5rem] ${
+                item.colorful ? "bg-conic-gradient" : "bg-n-6"
+              }`}
+              >
+                <div
+                  className="relative p-8 bg-n-8 rounded-[2rem] overflow-hidden
+                xl:p-15"
+                >
+                  <div className="absolute top-0 left-0 max-w-full">
                     <img
                       src={grid}
                       className="w-full"
@@ -28,14 +37,34 @@ const Roadmap = () => {
                   </div>
 
                   <div className="relative z-1">
-                    <div className="flex items-center justify-between">
+                    <div className="flex items-center justify-between max-w-[27rem] mb-8 md:mb-20">
                       <Tagline>{item.date}</Tagline>
                     </div>
 
                     <div>
-                      <img src={item.status === "done" ? check2 : loading1} />
+                      <img
+                        src={item.status === "done" ? check2 : loading1}
+                        className="mr-2.5"
+                        width={16}
+                        height={16}
+                        alt={status}
+                      />
+                      <div className="tagline">{status}</div>
                     </div>
                   </div>
+
+                  <div className="mb-10">
+                    <img
+                      src={item.imageUrl}
+                      className="w-full"
+                      width={630}
+                      height={420}
+                      alt={item.title}
+                    />
+                  </div>
+
+                  <h4 className="h4 mb-4">{item.title}</h4>
+                  <p className="body-2 text-n-4">{item.text}</p>
                 </div>
               </div>
             );
